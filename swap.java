24122435e1c2c35b692791421b846f38cc70ff72
HameedMulani <hameedmulani2001@gmail.com>
2021-09-13 16:02:23 +0530

    swap.java Modified bt Hameed

--- swap.java
+++ swap.java
@@ -3,13 +3,11 @@
  */
 import java.util.*;
 
-
 public class swap {
    public static void main(String[] args) {
     int a = 20, b = 30;
     swap(a, b);
    }
-
     private static void swap(int a, int b) {
         int temp;
         temp = a;
